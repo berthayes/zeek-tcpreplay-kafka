814437f1e9d6a8b696f0e586b7ad4d52eede5ada
Bert <bert@utexas.edu>
2020-05-16 15:56:42 -0500

    Adding a loop to tcpreplay

--- init_dummy.sh
+++ init_dummy.sh
@@ -4,6 +4,6 @@ ip link add dummy0 type dummy
 ifconfig dummy0 mtu 3000
 ifconfig dummy0 up
 /usr/local/zeek/bin/zeek -i dummy0 local "Site::local_nets += {192.168.1.0/24 }" &
-/usr/bin/tcpreplay -i dummy0 /pcaps/zeek_streamer.pcap &
+/usr/bin/tcpreplay -i dummy0 --loop=1000000 /pcaps/zeek_streamer.pcap &
 tail -f /dev/null
 exec "$@"
